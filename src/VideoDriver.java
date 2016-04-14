c9ec4562d823978dafa268cf7b7f0448956a6f3a
sverlingo <stephanie.hemmer@gmail.com>
2016-04-14 18:05:40 -0500

    updating driver

--- src/VideoDriver.java
+++ src/VideoDriver.java
@@ -2,16 +2,16 @@
 public class VideoDriver {
 
 	public static void main(String[] args) {
-		VideoInterface video1 = new ProxyVideo("video1.mov");
+		System.out.println("Creating new ProxyVideo");
+		VideoInterface video1 = new ProxyVideo("video1.mov"); 
 		
+		System.out.println("\nCreating new RealVideo");
 		VideoInterface video2 = new RealVideo("video2.mov");
-		
+		System.out.println("\nPlaying ProxyVideo");
 		video1.play();
-		
-		video1.play();
-		
+		System.out.println("\nPlaying realVideo");
 		video2.play();
-		
+		System.out.println("\nPlaying ProxyVideo");
 		video1.play();
 	}
 
